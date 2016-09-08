adea41c199cce8a6bd23974ee6872f730a61217a
Marcos P <ing.marcosp@icloud.com>
2016-09-08 15:16:09 +0200

    first commit
    s

---
+++ cousera-1-semana.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// mis test
+
+var cadena : String = "mi mundo"
+
+print(cadena)
+
+let pi : Float = 3.14
+
+var numero = 200
+
+print("una cadena de \(cadena)")
+var operadoresUnarios = 1
+
+operadoresUnarios += 1
+
+operadoresUnarios += 1
+
+operadoresUnarios -= 1
+
+print(operadoresUnarios)
+
+let numerox : Int = 874 % 10
+let operacion = 300 - 201 * 9 % 6 / 8
+
+let a = 5
+var f = 2
+let z = 3
+
+print("resultad \(a % ++f * z)")
